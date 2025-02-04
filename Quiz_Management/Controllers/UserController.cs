1981ba3e5d1e56316a3d3418198322daebef8701
@jainil@ <jainilthummar2005@gmail.com>
2025-02-04 15:31:29 +0530

    just created new repo

---
+++ Quiz_Management/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Quiz_Management.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult UserList()
+        {
+            return View();
+        }
+        public IActionResult AddUsers()
+        {
+            return View();
+        }
+    }
+}
