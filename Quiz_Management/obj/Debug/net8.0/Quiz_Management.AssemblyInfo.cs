e8629a95f46c97724696762e375702de12428d7f
@jainil@ <jainilthummar2005@gmail.com>
2025-04-24 06:41:58 +0530

    24-4-25

--- Quiz_Management/obj/Debug/net8.0/Quiz_Management.AssemblyInfo.cs
+++ Quiz_Management/obj/Debug/net8.0/Quiz_Management.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quiz_Management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65faf4a65b6930b08a556de49462deb31fba45c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df868cb70cb15f0a15f41d370690902d804c36c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quiz_Management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quiz_Management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
